070e29c2830e162e1d7b0c4dff7246847c6386de
lswarss <ls.warss98@gmail.com>
2020-01-16 22:01:19 +0100

    More work with the project

--- src/main/java/academy/learnprogramming/controller/DemoController.java
+++ src/main/java/academy/learnprogramming/controller/DemoController.java
@@ -1,16 +1,48 @@
 package academy.learnprogramming.controller;
 
+import academy.learnprogramming.service.DemoService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Slf4j
 @Controller
 public class DemoController {
 
+    // == fields ==
+    private final DemoService demoService;
+
+    //== constructors ==
+    @Autowired
+    public DemoController(DemoService demoService) {
+        this.demoService = demoService;
+    }
+
+    // == request methods ==
     //http://localhost:8080/todo-list/hello
     @ResponseBody
     @GetMapping("/hello")
     public String hello(){
         return "hello";
     }
+
+    //http://localhost:8080/todo-list/welcome
+    // prefix + name + suffix
+    // WEB-INF/view/welcome.jsp
+    @GetMapping("welcome")
+    public String welcome(Model model){
+        model.addAttribute("helloMessage", demoService.getHelloMessage("Luke"));
+        return "welcome";
+    }
+
+    // == model attributes
+    @ModelAttribute("welcomeMessage")
+    public String welcomeMessage(){
+        return demoService.getWelcomeMessage();
+    }
+
 }
