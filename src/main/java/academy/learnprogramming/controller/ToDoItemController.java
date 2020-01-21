756af0ac091895255a337895df9de5da825f359b
lswarss <ls.warss98@gmail.com>
2020-01-21 15:37:15 +0100

    Added the todo list controller

---
+++ src/main/java/academy/learnprogramming/controller/ToDoItemController.java
@@ -0,0 +1,31 @@
+package academy.learnprogramming.controller;
+
+import academy.learnprogramming.model.ToDoData;
+import academy.learnprogramming.util.Mappings;
+import academy.learnprogramming.util.ViewNames;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@Controller
+public class ToDoItemController {
+
+    //== model attributes ==
+    @ModelAttribute
+    public ToDoData toDoData(){
+        return new ToDoData();
+    }
+
+    // == handler methods ==
+
+    //http://localhost:8080/todo-list/items
+    @GetMapping(Mappings.ITEMS)
+    public String items(){
+        return ViewNames.ITEMS_LIST;
+    }
+
+
+
+
+
+}
--- src/main/java/academy/learnprogramming/model/ToDoItem.java
+++ src/main/java/academy/learnprogramming/model/ToDoItem.java
@@ -13,13 +13,13 @@ public class ToDoItem {
     private int id;
     private String title;
     private String details;
-    private LocalDate deadLine;
+    private LocalDate deadline;
 
     // == constructors ==
-    public ToDoItem(String title, String details, LocalDate deadLine) {
+    public ToDoItem(String title, String details, LocalDate deadline) {
         this.title = title;
         this.details = details;
-        this.deadLine = deadLine;
+        this.deadline = deadline;
     }
 
 }
