2883ea19fad0941b80606cf697e64feee4effeb4
lswarss <ls.warss98@gmail.com>
2020-01-25 11:20:55 +0100

    Added the add the task j

--- src/main/java/academy/learnprogramming/controller/ToDoItemController.java
+++ src/main/java/academy/learnprogramming/controller/ToDoItemController.java
@@ -6,12 +6,18 @@ import academy.learnprogramming.service.ToDoItemService;
 import academy.learnprogramming.util.AttributeNames;
 import academy.learnprogramming.util.Mappings;
 import academy.learnprogramming.util.ViewNames;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDate;
+
+@Slf4j
 @Controller
 public class ToDoItemController {
     // == fields ==
@@ -36,9 +42,26 @@ public class ToDoItemController {
         return ViewNames.ITEMS_LIST;
     }
 
+    @GetMapping(Mappings.ADD_ITEM)
+    public String addEditItem(Model model){
+        ToDoItem toDoItem = new ToDoItem("", "", LocalDate.now());
+        model.addAttribute(AttributeNames.TODO_ITEM, toDoItem);
+        return ViewNames.ADD_ITEM;
+    }
+
+
     @PostMapping(Mappings.ADD_ITEM)
     public String processItem(@ModelAttribute(AttributeNames.TODO_ITEM) ToDoItem toDoItem){
+        log.info("toDoItem from form = {}", toDoItem);
+        toDoItemService.addItem(toDoItem);
         return "redirect:/"+ Mappings.ITEMS;
+    }
+
+    @GetMapping(Mappings.DELETE_ITEM)
+    public String deleteItem(@RequestParam int id){
+        log.info("toDoItem removed form form = {}", id);
+        toDoItemService.removeItem(id);
+        return "redirect:/" + Mappings.ITEMS;
     }
 
 
---
+++ src/main/webapp/WEB-INF/view/home.jsp
@@ -0,0 +1,22 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: lswar
+  Date: 25/01/2020
+  Time: 10:43
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page import="academy.learnprogramming.util.Mappings" %>
+<html>
+<head>
+    <title>Todo List Application</title>
+</head>
+<body>
+<div align="center">
+    <c:url var="itemsLink" value="${Mappings.ITEMS}"/>
+    <h2><a href="${itemsLink}">Show Todo Items</a></h2>
+</div>
+
+</body>
+</html>
